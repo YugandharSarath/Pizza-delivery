faaeb55eca188034fc75e885cdcccb6f8025a3fc
Yugandhar <yugandhar.sarath53@gmail.com>
2022-01-10 18:28:07 +0530

    second commit

---
+++ components/PizzaList.jsx
@@ -0,0 +1,21 @@
+import styles from "../styles/PizzaList.module.css";
+import PizzaCard from "./PizzaCard";
+
+const PizzaList = ({ pizzaList }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>THE BEST PIZZA IN TOWN</h1>
+      <p className={styles.desc}>
+        This is a yum & delicious homemade paneer pizza made with toppings of paneer 
+        and mix veggies. For the pizza sauce, you can also add store brought tomato pizza sauce or any other.
+      </p>
+      <div className={styles.wrapper}>
+        {pizzaList.map((pizza) => (
+          <PizzaCard key={pizza._id} pizza={pizza} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PizzaList;
